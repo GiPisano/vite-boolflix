29cdf7c2868eb6838e154993645098cc8b0239d1
GiPisano <pisanogiacomo9@gmail.com>
2024-02-14 15:55:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import { store } from './store';
+
+export default{
+  data(){
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Hello BoolFlix</h1>
+</template>
+
+<style>
+
+</style>
