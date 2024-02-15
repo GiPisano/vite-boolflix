f9cde82080ac25500fcb4b4f41e623af267e95c7
GiPisano <pisanogiacomo9@gmail.com>
2024-02-15 16:28:19 +0100

    add Milestone 2

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,36 @@ export default{
     return {
       store,
       inputValue: '',
-      films: '',
+      movies: '',
+      tvSeries: '',
     }
   },
 
   methods:{
+    fetchMovies(){
+      axios.get(`${store.uri.apiUri}search/movie?api_key=${store.uri.api_key}&query=${this.inputValue}`).then((res) => {
+      this.movies = res.data.results;
+      })
+    },
+
+    fetchTvSeries(){
+      axios.get(`${store.uri.apiUri}search/tv?api_key=${store.uri.api_key}&query=${this.inputValue}`).then((res) => {
+      this.tvSeries = res.data.results;
+      })
+    },
+
     search(){
-      axios.get(`${store.apiUri}query=${this.inputValue}`).then((res) => {
-      this.films = res.data.results;
-    })
+      this.fetchMovies(),
+      this.fetchTvSeries()
+    },
+
+    iconsflag(movie){
+      if(movie.original_language == 'en'){return './src/assets/flags/en.png'}
+      if(movie.original_language == 'de'){return './src/assets/flags/de.png'}
+      if(movie.original_language == 'fr'){return './src/assets/flags/fr.png'}
+      if(movie.original_language == 'it'){return './src/assets/flags/it.png'}
+      if(movie.original_language == 'ja'){return './src/assets/flags/ja.png'}
+      else{return './src/assets/flags/generalFlag.png'}
     }
   },
 
@@ -28,19 +49,37 @@ export default{
 <template>
   <h1>Hello BoolFlix</h1>
 
-  <input type="text" v-model="inputValue">
+  <input type="text" v-model="inputValue" @keyup="search()">
   <button @click="search()">Cerca</button>
 
-  <ul v-for="film in films">
-    <li>{{ film.title }}</li>
-    <li>{{ film.original_title }}</li>
-    <li>{{ film.original_language}}</li>
-    <li>{{ film.vote_average }}</li>
+  <ul v-for="movie in movies">
+    <li>Titolo film: {{ movie.title }}</li>
+    <li>Titolo originale film: {{ movie.original_title }}</li>
+    <li class="img-flag">
+      <span>Lingua film: </span>
+      <img :src="iconsflag(movie)" alt="">
+    </li>
+    <li>Voto film: {{ movie.vote_average }}</li>
+  </ul>
+
+  <ul v-for="tvSerie in tvSeries">
+    <li>Nome serie tv: {{ tvSerie.name }}</li>
+    <li>Nome originale serie tv: {{ tvSerie.original_name }}</li>
+    <li class="img-flag">
+      <span>Lingua serie tv: </span>
+      <img :src="iconsflag(tvSerie)" alt="">
+    </li>
+    <li>Voto serie tv: {{ tvSerie.vote_average }}</li>
   </ul>
 
  
 </template>
 
-<style>
-
+<style lang="scss">
+.img-flag{
+  img{
+    max-width: 30px;
+  }
+ 
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-  apiUri: 'https://api.themoviedb.org/3/search/movie?api_key=e5070052f9ec6dfe5bcf6c3d61c62c05&'
+  uri:{
+    apiUri:'https://api.themoviedb.org/3/',
+    api_key: 'e5070052f9ec6dfe5bcf6c3d61c62c05'
+  },
 });
-
