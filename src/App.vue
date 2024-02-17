0801142ff68ca8d1d51f534e2e9ead256018b9e6
GiPisano <pisanogiacomo9@gmail.com>
2024-02-17 12:50:34 +0100

    add app header, code cleanup and improvement

--- src/App.vue
+++ src/App.vue
@@ -2,68 +2,94 @@
 import { store } from './store';
 import axios from 'axios';
 
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue'
+
 export default{
   data(){
     return {
       store,
-      inputValue: '',
-      movies: '',
-      tvSeries: '',
     }
   },
 
-  methods:{
+  components:{ AppHeader, AppMain },
 
+  methods:{
     // richiesta axois per film e serie tv
-    fetchMovies(){
-      axios.get(`${store.uri.apiUri}search/movie?api_key=${store.uri.api_key}&query=${this.inputValue}`).then((res) => {
-      this.movies = res.data.results;
+    fetchMovies(inputValue){
+      axios.get(`${store.api.apiUri}/search/movie`, {
+        params: {
+          api_key: store.api.key,
+          query: inputValue,
+        },
+      })
+      .then((res) => {
+      store.movies = res.data.results.map((movie) => {
+        return{
+          title: movie.title,
+          original_title: movie.original_title,
+          language: movie.original_language,
+          vote: Math.floor(movie.vote_average / 2) + 1,
+          poster_path: movie.poster_path
+        }
+      });
       })
     },
 
-    fetchTvSeries(){
-      axios.get(`${store.uri.apiUri}search/tv?api_key=${store.uri.api_key}&query=${this.inputValue}`).then((res) => {
-      this.tvSeries = res.data.results;
+    fetchTvSeries(inputValue){
+      axios.get(`${store.api.apiUri}/search/tv`, {
+        params: {
+          api_key: store.api.key,
+          query: inputValue,
+        },
+      })
+      .then((res) => {
+      store.tvSeries = res.data.results.map((tvSerie) => {
+        return{
+          title: tvSerie.name,
+          original_title: tvSerie.original_name,
+          language: tvSerie.original_language,
+          vote: Math.floor(tvSerie.vote_average / 2) + 1,
+          poster_path: tvSerie.poster_path
+        }
+      });
       })
     },
 
     // generazioine copertina film e serie tv
     pathImg(movie){
-     return `${store.uri.imgPhat}${movie.poster_path}`
+     return `${store.api.imgPhat}${movie.poster_path}`
     },
 
     // funzione dell'input 
-    search(){
-      this.fetchMovies(),
-      this.fetchTvSeries()
-    },
-
-    // genero stelle in base al voto
-    stars(movie){
-      return Math.floor(movie.vote_average / 2) + 1;
+    performSearch(inputValue){
+      this.fetchMovies(inputValue),
+      this.fetchTvSeries(inputValue)
     },
 
     // bandiere lingua 
     iconsflag(movie){
-      if(movie.original_language == 'en'){return './src/assets/flags/en.png'}
-      if(movie.original_language == 'de'){return './src/assets/flags/de.png'}
-      if(movie.original_language == 'fr'){return './src/assets/flags/fr.png'}
-      if(movie.original_language == 'it'){return './src/assets/flags/it.png'}
-      if(movie.original_language == 'ja'){return './src/assets/flags/ja.png'}
+      if(movie.language == 'en'){return './src/assets/flags/en.png'}
+      if(movie.language == 'de'){return './src/assets/flags/de.png'}
+      if(movie.language == 'fr'){return './src/assets/flags/fr.png'}
+      if(movie.language == 'it'){return './src/assets/flags/it.png'}
+      if(movie.language == 'ja'){return './src/assets/flags/ja.png'}
       else{return './src/assets/flags/generalFlag.png'}
     }
   },
+
+
 }
 </script>
 
+
+
 <template>
 
-  <!-- header -->
-  <input type="text" v-model="inputValue" @keyup="search()">
-  <button @click="search()">Cerca</button>
+  <app-header @search="performSearch"></app-header> 
 
   <!-- film section -->
-  <ul v-for="movie in movies">
+  <ul v-for="movie in store.movies">
     <img :src="pathImg(movie)" alt="">
     <li>Titolo film: {{ movie.title }}</li>
     <li>Titolo originale film: {{ movie.original_title }}</li>
@@ -71,26 +97,26 @@ export default{
       <span>Lingua film: </span>
       <img :src="iconsflag(movie)" alt="">
     </li>
-    <li>Voto film: {{ stars(movie) }}
+    <li>Voto film: {{ movie.vote }}
       <span v-for="index in 5">
-        <i v-if="index <= stars(movie)" class="fa-solid fa-star star"></i>
+        <i v-if="index <= movie.vote" class="fa-solid fa-star star"></i>
         <i v-else class="fa-regular fa-star star"></i>
       </span>
     </li>
   </ul>
 
   <!-- serie section -->
-  <ul v-for="tvSerie in tvSeries">
+  <ul v-for="tvSerie in store.tvSeries">
     <img :src="pathImg(tvSerie)" alt="" v-show="pathImg(tvSerie)">
-    <li>Nome serie tv: {{ tvSerie.name }}</li>
-    <li>Nome originale serie tv: {{ tvSerie.original_name }}</li>
+    <li>Nome serie tv: {{ tvSerie.title }}</li>
+    <li>Nome originale serie tv: {{ tvSerie.original_title }}</li>
     <li class="img-flag">
       <span>Lingua serie tv: </span>
       <img :src="iconsflag(tvSerie)" alt="">
     </li>
-    <li>Voto serie tv: {{ stars(tvSerie) }}
+    <li>Voto serie tv: {{ tvSerie.vote }}
       <span v-for="index in 5">
-        <i v-if="index <= stars(tvSerie)" class="fa-solid fa-star star"></i>
+        <i v-if="index <= tvSerie.vote" class="fa-solid fa-star star"></i>
         <i v-else class="fa-regular fa-star star"></i>
       </span>
     </li>
@@ -100,6 +126,8 @@ export default{
 </template>
 
 <style lang="scss">
+@use './styles/general.scss';
+
 .img-flag{
   img{
     max-width: 30px;
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script>
+    export default {
+        data(){
+            return {
+                inputValue: '',
+            }
+        },
+
+        emits: ['search'],
+    }
+</script>
+
+<template>
+    <div class="header">
+        <h2>Boolflix</h2>
+
+        <div>
+            <input v-model="inputValue" @keyup="$emit('search', this.inputValue)" type="text" placeholder="Search..">
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+
+    background-color: black;
+    color: white;
+
+    h2{
+        color: red;
+        font-size: 30px;
+        font-family: Georgia, serif;
+    }
+
+    input{
+        width: 200px;
+        height: 30px;
+        font-size: 15px;
+        padding: 5px;
+    }
+}
+</style>
